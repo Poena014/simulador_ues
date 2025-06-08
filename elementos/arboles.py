2c07ae96cfb3be304d157f6938c0547400e918e0
Vanessa <vanesitacarranzamc@gmail.com>
2025-06-08 16:03:03 -0600

    cambio de arbol

--- elementos/arboles.py
+++ elementos/arboles.py
@@ -5,7 +5,7 @@ def cargar_arboles():
     
     # Definición de los arboles
     
-    arbol_izquierda_1 = Entity(
+    arbol_1 = Entity(
         model='tripo_convert_aba66a69-45fd-4baa-b625-7ae8a34771e7.obj',
         texture=load_texture('tripo_image_aba66a69-45fd-4baa-b625-7ae8a34771e7_0.jpg'),
         scale=2.5,
@@ -13,7 +13,7 @@ def cargar_arboles():
         rotation_y=0
     )
     
-    arboles.append(arbol_izquierda_1)
+    arboles.append(arbol_1)
 
     arbol_izquierda_2 = Entity(
         model='tripo_convert_b68a70a1-05d1-449b-ac0e-2b2583790c71.obj',
